80e5dd5b6d5aad52073c1aea8f57fe83fa43fbc4
Frank Bosma <frank@frankbosma.nl>
2017-03-21 15:00:32 +0100

    Adding Unity project

---
+++ README.md
@@ -0,0 +1,3 @@
+# contents
+
+`STRP` is de Unity app. Zelf even builden naar een Visual Studio Project.
---
+++ STRP/Assets/HologramManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class HologramManager : MonoBehaviour
+{
+
+    public Button dataFetcher;
+    public Text dataDisplayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("HololensStarted");
+        StartCoroutine(GetText());
+    }
+
+    void doGetText()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://ip169-109.vpro.nl:8080/list/"))
+        {
+            yield return www.Send();
+
+            if (www.isError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                if (dataDisplayer != null)
+                {
+                    dataDisplayer.text = www.downloadHandler.text;
+                }
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+}
